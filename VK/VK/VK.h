22b37b274eacc7de988731adfbbb51e8d3f7dda1
horinoh <development@horino.sakura.ne.jp>
2024-09-20 01:23:40 +0900

    更新

--- VK/VK/VK.h
+++ VK/VK/VK.h
@@ -22,12 +22,20 @@
 #pragma warning(disable:4244)
 #pragma warning(disable:4458)
 #pragma warning(disable:5054)
+#else
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wignored-qualifiers"
+#pragma GCC diagnostic ignored "-Wtype-limits"
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+#pragma GCC diagnostic ignored "-Wdeprecated-enum-enum-conversion"
 #endif
 #include <gli/gli.hpp>
 #include <gli/target.hpp>
 #ifdef _WIN64
 #pragma warning(pop)
 #pragma comment(lib, "vulkan-1.lib")
+#else
+#pragma GCC diagnostic pop
 #endif
 
 #ifdef _DEBUG
